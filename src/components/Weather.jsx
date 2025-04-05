115938054f8cd97dc68b8633e88cd120106698ea
Demo <niteshkumar42655@gmail.com>
2025-04-05 16:17:32 +0530

    add files

---
+++ src/components/Weather.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState, useRef } from 'react';
+import './Weather.css';
+import searchIcon from '../assets/search.png';
+import clear from '../assets/clear.png';
+import cloud from '../assets/cloud.png';
+import drizzle from '../assets/drizzle.png';
+import humidity from '../assets/humidity.png';
+import rain from '../assets/rain.png';
+import snow from '../assets/snow.png';
+import wind from '../assets/wind.png';
+
+const Weather = () => {
+    const inputRef = useRef();
+    const [weatherData, setWeatherData] = useState(null);
+
+    const allIcons = {
+        "01d": clear,
+        "01n": clear,
+        "02d": cloud,
+        "02n": cloud,
+        "03d": cloud,
+        "03n": cloud,
+        "04d": drizzle,
+        "04n": drizzle,
+        "09d": rain,
+        "09n": rain,
+        "10d": rain,
+        "10n": rain,
+        "13d": snow,
+        "13n": snow
+    };
+
+    const fetchWeather = async (city) => {
+        if (city ===""){
+            alert("Enter the city name");
+          return;
+        } 
+        
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+
+            if (!data || data.cod !== 200) {
+                console.error("Invalid city or API error:", data);
+                setWeatherData(null);
+                return;
+            }
+
+            const icon = allIcons[data.weather[0].icon] || clear;
+
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            });
+        } catch (error) {
+            setWeatherData(false);
+            console.error("Error fetching weather data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchWeather("London");
+    }, []);
+
+    return (
+        <div className='weather'>
+            <div className="search-bar">
+                <input ref={inputRef} type="text" placeholder='Search city...' />
+                <img src={searchIcon} alt="search" onClick={() => fetchWeather(inputRef.current.value)} />
+            </div>
+
+            {weatherData ? (
+                <>
+                    <img src={weatherData.icon} alt="Weather Icon" className='weather-icon' />
+                    <p className='temperature'>{weatherData.temperature}°C</p>
+                    <p className='location'>{weatherData.location}</p>
+                    <div className='weather-data'>
+                        <div className="col">
+                            <img src={humidity} alt="Humidity" />
+                            <div>
+                                <p>{weatherData.humidity}%</p>
+                                <span>Humidity</span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img src={wind} alt="Wind Speed" />
+                            <div>
+                                <p>{weatherData.windSpeed} km/h</p>
+                                <span>Wind Speed</span>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <p>Loading weather...</p>
+            )}
+        </div>
+    );
+};
+
+export default Weather;
